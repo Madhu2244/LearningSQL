c061f0b201026e2fa15db295f212131b605f0f6e
Madhu2244 <63474175+Madhu2244@users.noreply.github.com>
2020-07-06 15:20:37 -0500

    Add files via upload
    Adding SQL File That i Made Today

---
+++ student.sql
@@ -0,0 +1,39 @@
+CREATE TABLE STUDENT (student_id INTEGER, student_name  varchar(30), student_phone varchar(15), student_address varchar(50), class_id INTEGER,
+PRIMARY KEY (student_id)
+);
+
+CREATE TABLE PROFESSOR (professor_id INTEGER PRIMARY KEY, professor_name varchar(30), professor_phone varchar(15));
+
+CREATE TABLE CLASS (class_id INTEGER PRIMARY KEY, class_name varchar(30), professor_id INTEGER);
+
+ALTER TABLE CLASS 
+ADD FOREIGN KEY (professor_id) references PROFESSOR(professor_id);
+
+ALTER TABLE STUDENT
+ADD FOREIGN KEY (class_id) references CLASS(class_id);
+
+INSERT INTO PROFESSOR VALUE(1, "Mr. Sharma", "123-123-1234");
+INSERT INTO PROFESSOR VALUE(2, "Mr. William", "321-321-3210");
+INSERT INTO PROFESSOR VALUE(3, "Mr. Jake", "234-234-2345");
+
+INSERT INTO CLASS VALUE(1, "Math", 1);
+INSERT INTO CLASS VALUE(2, "English", 1);
+INSERT INTO CLASS VALUE(3, "Science", 2);
+INSERT INTO CLASS VALUE(4, "History", 3);
+
+INSERT INTO STUDENT VALUE(12,"Madhusudhan ", "123-123-1233","2020 Youth Road", 1);
+INSERT INTO STUDENT VALUE(13,"William ", "123-123-1233","4040 Adult Road", 2);
+INSERT INTO STUDENT VALUE(14,"Josiah Vo", "123-123-1233","5050 Fast Road", 3);
+INSERT INTO STUDENT VALUE(15,"Blake", "123-123-1233","5050 Fast Road", 4);
+
+SELECT * FROM STUDENT;
+
+SELECT * FROM PROFESSOR;
+
+SELECT * FROM CLASS;
+
+SELECT * FROM STUDENT as s, CLASS as c, PROFESSOR as p where c.professor_id=p.professor_id and c.class_id = s.class_id;
+
+drop table PROFESSOR;
+drop table STUDENT;
+drop table CLASS;
